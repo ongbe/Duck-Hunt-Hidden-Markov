198e4f80515a0852f6a54cc27ed046a0746992fd
sootn <encrypt.wave@gmail.com>
2014-10-08 23:09:50 +0200

    Do over

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,80 +4,34 @@
 #include <cstdlib>
 #include "Matrix.hpp"
 #include <iostream>
+#include "../lib/Constants.hpp"
+
 
 namespace ducks
 {
 
-	Player::Player() {}
+	vector<HMM> species;
+	vector<int> species_map(COUNT_SPECIES);
+	vector<int> known_birds;
+	int round = -1;
+
+	Player::Player() {
+		Matrix<> iniA(COUNT_SPECIES, COUNT_SPECIES, 1.0/COUNT_SPECIES);		// TODO Randomize for better convergence
+		Matrix<> iniB(COUNT_SPECIES, COUNT_MOVE, (double) 1 / COUNT_MOVE);
+		Matrix<> iniq(1, COUNT_MOVE, 1.0/COUNT_MOVE);
+
+		for(int i = 0; i < COUNT_SPECIES; i++) {
+			HMM hmm(iniA, iniB, iniq);
+			species.push_back(hmm);
+		}
+	}
 
 	Action Player::shoot(const GameState &pState, const Deadline &pDue) {
-		int N = pState.getNumBirds();
-		int M = pState.getBird(0).getSeqLength();			// Keep an eye on! Could be wrong.
-
-		cerr << "Round: " <<  pState.getRound() << " nBirds: " << N << " nSequences: " << M << " myScore: " << pState.myScore() << endl;
-
-		if(M < 25) {
-			return cDontShoot;
+		if(round != pState.getRound()) {
+			cerr << "Round " << pState.getRound() << " with " << pState.getNumBirds() << " birds" << endl;
+			known_birds = vector<int>(pState.getNumBirds(), 0);
 		}
-
-		int npo = 4 + COUNT_MOVE - 1;
-
-		Matrix<> iniA(npo, npo, 1.0/COUNT_MOVE);
-		Matrix<> iniB(npo, COUNT_MOVE, 1E-100);
-
-		for(int i = 0; i < COUNT_MOVE; i++) {
-			if(i == MOVE_STOPPED) continue;
-			if(i < MOVE_STOPPED) {
-				iniB[i][i] = 1;
-			} else {
-				iniB[i-1][i] = 1;
-			}
-		}
-
-		iniB[COUNT_MOVE + 1 -2][MOVE_UP_LEFT] = 0.5;
-		iniB[COUNT_MOVE + 1 -2][MOVE_UP_RIGHT] = 0.5;
-		iniB[COUNT_MOVE + 2 -2][MOVE_DOWN] = 1.0/3;
-		iniB[COUNT_MOVE + 2 -2][MOVE_RIGHT] = 1.0/3;
-		iniB[COUNT_MOVE + 2 -2][MOVE_LEFT] = 1.0/3;
-		iniB[COUNT_MOVE + 3 -2] = vector<double>(COUNT_MOVE, 1.0/COUNT_MOVE);
-		iniB[COUNT_MOVE + 4 -2] = vector<double>(COUNT_MOVE, 1.0/COUNT_MOVE);
-
-		vector<double> quality;
-		vector<HMM> hmms;
-		for(int i = 0; i < N; i++) {
-			Bird bird = pState.getBird(i);
-			Matrix<> q(1, npo, 1.0/npo);
-			HMM model(iniA, iniB, q);
-			HMM::Sequence observations;
-			for(int j = 0; j < M; j++) {
-				observations.push_back(bird.getObservation(j));
-			}
-			BaumWelch bw(&model, observations);
-			double h = bw.train();
-			quality.push_back(h);
-			hmms.push_back(model);
-		}
-
-		int bestbird = -1;
-		double max = 0;
-		for(int i = 0; i < quality.size(); i++) {
-			if(quality[i] > max) {
-				max = quality[i];
-				bestbird = i;
-			}
-		}
-
-		Matrix<> next = hmms[bestbird].nextemission();
-		max = 0;
-		int bestmove = -1;
-		for(int i = 0; i < next.m(); i++) {
-			if(next[0][i] > max) {
-				max = next[0][i];
-				bestmove = i;
-			}
-		}
-
-		return Action(bestbird, (EMovement) bestmove);
+		return cDontShoot;
 	}
 
 	std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDue) {
@@ -86,7 +40,7 @@ namespace ducks
 		 * This skeleton makes no guesses, better safe than sorry!
 		 */
 		cerr << "Question" << endl;
-		std::vector<ESpecies> lGuesses(pState.getNumBirds(), SPECIES_UNKNOWN);
+		std::vector<ESpecies> lGuesses(pState.getNumBirds(), SPECIES_PIGEON);
 		return lGuesses;
 	}
 
@@ -102,6 +56,9 @@ namespace ducks
 		 * If you made any guesses, you will find out the true species of those birds in this function.
 		 */
 		cerr << "REVeal!" << endl;
+		for (int i = 0; i < pSpecies.size(); ++i) {
+			cerr << pSpecies[i] << " ";
+		}
 	}
 
 
